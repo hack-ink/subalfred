6e5ceaa383405a0bb9147ffd79d1652dd37a395b
Xavier Lau <c.estlavie@icloud.com>
2020-11-22 21:55:00 +0800

    backup

--- subhasher/src/lib.rs
+++ subhasher/src/lib.rs
@@ -1,6 +1,10 @@
+// --- std ---
+use std::hash::Hasher;
 // --- crates.io ---
 use array_bytes::*;
-use subhasher::*;
+use blake2_rfc::blake2b::blake2b;
+use byteorder::{ByteOrder, LittleEndian};
+use twox_hash::XxHash;
 
 pub fn hash(data: &str, hasher: &str, is_hex: bool) -> String {
 	let (data, bytes) = if is_hex {
@@ -43,4 +47,65 @@ pub fn parse_storage_keys(module: Option<&str>, item: Option<&str>) -> String {
 	}
 
 	storage_prefix
+}
+
+pub fn blake2_128(data: &[u8]) -> [u8; 16] {
+	let mut dest = [0; 16];
+	dest.copy_from_slice(blake2b(16, &[], data).as_bytes());
+
+	dest
+}
+
+pub fn blake2_256(data: &[u8]) -> [u8; 32] {
+	let mut dest = [0; 32];
+	dest.copy_from_slice(blake2b(32, &[], data).as_bytes());
+
+	dest
+}
+
+pub fn blake2_128_concat(data: &[u8]) -> Vec<u8> {
+	let mut v = blake2_128(data).to_vec();
+	v.extend_from_slice(data);
+
+	v
+}
+
+fn twox(dest: &mut [u8], data: &[u8], seed: u64) {
+	let mut h = XxHash::with_seed(seed);
+	let i = seed as usize * 8;
+
+	h.write(data);
+	LittleEndian::write_u64(&mut dest[i..i + 8], h.finish());
+}
+
+pub fn twox_64(data: &[u8]) -> [u8; 8] {
+	let mut dest = [0; 8];
+	twox(&mut dest, data, 0);
+
+	dest
+}
+
+pub fn twox_128(data: &[u8]) -> [u8; 16] {
+	let mut dest = [0; 16];
+	twox(&mut dest, data, 0);
+	twox(&mut dest, data, 1);
+
+	dest
+}
+
+pub fn twox_256(data: &[u8]) -> [u8; 32] {
+	let mut dest = [0; 32];
+	twox(&mut dest, data, 0);
+	twox(&mut dest, data, 1);
+	twox(&mut dest, data, 2);
+	twox(&mut dest, data, 3);
+
+	dest
+}
+
+pub fn twox_64_concat(data: &[u8]) -> Vec<u8> {
+	let mut v = twox_64(data).to_vec();
+	v.extend_from_slice(data);
+
+	v
 }
