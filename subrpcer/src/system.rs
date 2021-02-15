cf824775556d8618f2e3581fed4af26d6f808aae
Xavier Lau <c.estlavie@icloud.com>
2021-02-15 20:02:31 +0800

    backup

--- subrpcer/src/system.rs
+++ subrpcer/src/system.rs
@@ -1,6 +1,6 @@
 // --- crates.io ---
 use serde::Serialize;
-use serde_json::{json, Value};
+use serde_json::Value;
 // --- subrpcer ---
 use crate::{rpc, DEFAULT_ID};
 
@@ -8,5 +8,5 @@ pub fn properties_with_id(id: impl Serialize) -> Value {
 	rpc("system_properties", Value::Null, id)
 }
 pub fn properties() -> Value {
-	get_metadata_with_id(DEFAULT_ID)
+	properties_with_id(DEFAULT_ID)
 }
