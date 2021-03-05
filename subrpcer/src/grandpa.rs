e4e6ca2923fcc750313331e5c0a47e9bb2e2bf98
Xavier Lau <c.estlavie@icloud.com>
2021-03-05 13:16:56 +0800

    backup

--- subrpcer/src/grandpa.rs
+++ subrpcer/src/grandpa.rs
@@ -4,6 +4,13 @@ use serde_json::Value;
 // --- subrpcer ---
 use crate::{rpc, DEFAULT_ID};
 
+pub fn round_state_with_id(id: impl Serialize) -> Value {
+	rpc("grandpa_roundState", Value::Null, id)
+}
+pub fn round_state() -> Value {
+	round_state_with_id(DEFAULT_ID)
+}
+
 pub fn subscribe_justifications_with_id(id: impl Serialize) -> Value {
 	rpc("grandpa_subscribeJustifications", Value::Null, id)
 }
