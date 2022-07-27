36983419be18f52640413648739619600dd9f5fc
Xavier Lau <xavier@inv.cafe>
2022-07-27 23:47:13 +0800

    Support Byte String Literal

--- src/bin/subalfred/command/convert/hex2bytes.rs
+++ src/bin/subalfred/command/convert/hex2bytes.rs
@@ -1,3 +1,5 @@
+// std
+use std::fmt::Write;
 // crates.io
 use clap::Args;
 // hack-ink
@@ -11,14 +13,36 @@ pub(crate) struct Hex2BytesCmd {
 	/// Example: `0x00000000`.
 	#[clap(required = true, value_name = "HEX")]
 	hex: String,
+	/// Byte string literal style.
+	#[clap(long, takes_value = false)]
+	byte_string_literal: bool,
 }
 impl Hex2BytesCmd {
 	#[tokio::main]
 	pub(crate) async fn run(&self) -> Result<()> {
-		let Self { hex } = self;
+		let Self { hex, byte_string_literal } = self;
+		let quick_err = || quick_err("invalid byte string");
+		let bytes = array_bytes::hex2bytes(hex).map_err(|_| quick_err())?;
 
-		println!("{:?}", array_bytes::hex2bytes(hex).map_err(|_| quick_err("invalid hex input"))?);
+		if *byte_string_literal {
+			println!("{}", try_as_byte_string_literal(&bytes).ok_or(quick_err())?);
+		} else {
+			println!("{bytes:?}");
+		}
 
 		Ok(())
 	}
+}
+
+fn try_as_byte_string_literal(bytes: &[u8]) -> Option<String> {
+	let mut byte_string_literal = String::new();
+
+	for &byte in bytes {
+		match byte {
+			40..=126 => byte_string_literal.push(std::char::from_u32(byte as u32).unwrap()),
+			_ => write!(byte_string_literal, "\\x{byte:02X}").ok()?,
+		}
+	}
+
+	Some(byte_string_literal)
 }
