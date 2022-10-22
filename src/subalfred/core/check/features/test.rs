1ce38ba230f5435357a62a55aedb90c1553f3092
Xavier Lau <xavier@inv.cafe>
2022-10-22 13:58:30 +0800

    Add test

--- src/subalfred/core/check/features/test.rs
+++ src/subalfred/core/check/features/test.rs
@@ -1,3 +1,14 @@
-// TODO: add testcase
+// hack-ink
+use super::*;
+
 #[test]
-fn check_should_work() {}
+fn check_mock_runtime_should_work() {
+	assert_eq!(
+		check("src/subalfred/core/check/features/mock-runtime/Cargo.toml").unwrap(),
+		vec![
+			("std".to_string(), vec!["pallet-a".to_string(), "primitive-a".to_string()]),
+			("runtime-benchmarks".to_string(), vec!["pallet-b".to_string()]),
+			("try-runtime".to_string(), vec!["pallet-c".to_string()]),
+		]
+	);
+}
