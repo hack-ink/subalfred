5fcfdeae9c03a64cb9f5c405b005b16cb9c1b4a2
Xavier Lau <xavier@inv.cafe>
2022-10-22 14:09:52 +0800

    Fix test

--- src/subalfred/core/check/features/test.rs
+++ src/subalfred/core/check/features/test.rs
@@ -3,12 +3,22 @@ use super::*;
 
 #[test]
 fn check_mock_runtime_should_work() {
-	assert_eq!(
-		check("src/subalfred/core/check/features/mock-runtime/Cargo.toml").unwrap(),
-		vec![
-			("std".to_string(), vec!["pallet-a".to_string(), "primitive-a".to_string()]),
-			("runtime-benchmarks".to_string(), vec!["pallet-b".to_string()]),
-			("try-runtime".to_string(), vec!["pallet-c".to_string()]),
-		]
-	);
+	let mut result = check("src/subalfred/core/check/features/mock-runtime/Cargo.toml").unwrap();
+	let mut expect_result = {
+		let result: &[(&str, &[&str])] = &[
+			("std", &["pallet-a", "primitive-a"]),
+			("runtime-benchmarks", &["pallet-b"]),
+			("try-runtime", &["pallet-c"]),
+		];
+
+		result
+			.iter()
+			.map(|(a, b)| (a.to_string(), b.iter().map(ToString::to_string).collect()))
+			.collect::<Vec<_>>()
+	};
+
+	result.sort();
+	expect_result.sort();
+
+	assert_eq!(result, expect_result);
 }
