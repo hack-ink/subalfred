b57b640211f55ecf23d7ea1e0d47e6f39c96e4f9
Xavier Lau <xavier@inv.cafe>
2022-10-28 21:08:14 +0800

    Doc `subhasher`

--- substrate-minimal/subhasher/src/lib.rs
+++ substrate-minimal/subhasher/src/lib.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+#![warn(missing_docs)]
 
 #[cfg(not(feature = "std"))] extern crate alloc;
 
@@ -13,6 +14,7 @@ use sha2::{Digest, Sha256};
 use tiny_keccak::{Hasher as _, Keccak};
 use twox_hash::XxHash;
 
+/// Hash the data into 16-bytes with BLAKE2 algorithm.
 pub fn blake2_128(data: &[u8]) -> [u8; 16] {
 	let mut dest = [0; 16];
 	dest.copy_from_slice(blake2b(16, &[], data).as_bytes());
@@ -20,6 +22,7 @@ pub fn blake2_128(data: &[u8]) -> [u8; 16] {
 	dest
 }
 
+/// Hash the data into 32-bytes with BLAKE2 algorithm.
 pub fn blake2_256(data: &[u8]) -> [u8; 32] {
 	let mut dest = [0; 32];
 	dest.copy_from_slice(blake2b(32, &[], data).as_bytes());
@@ -27,6 +30,7 @@ pub fn blake2_256(data: &[u8]) -> [u8; 32] {
 	dest
 }
 
+/// Hash the data into 64-bytes with BLAKE2 algorithm.
 pub fn blake2_512(data: &[u8]) -> [u8; 64] {
 	let mut dest = [0; 64];
 	dest.copy_from_slice(blake2b(64, &[], data).as_bytes());
@@ -34,6 +38,7 @@ pub fn blake2_512(data: &[u8]) -> [u8; 64] {
 	dest
 }
 
+/// Hash the data into `blake2_128(data) + data`.
 pub fn blake2_128_concat(data: &[u8]) -> Vec<u8> {
 	let data = data;
 	let mut v = blake2_128(data).to_vec();
@@ -50,6 +55,7 @@ fn twox(dest: &mut [u8], data: &[u8], seed: u64) {
 	LittleEndian::write_u64(&mut dest[i..i + 8], h.finish());
 }
 
+/// Hash the data into 8-bytes with XX algorithm.
 pub fn twox64(data: &[u8]) -> [u8; 8] {
 	let mut dest = [0; 8];
 	twox(&mut dest, data, 0);
@@ -57,6 +63,7 @@ pub fn twox64(data: &[u8]) -> [u8; 8] {
 	dest
 }
 
+/// Hash the data into 16-bytes with XX algorithm.
 pub fn twox128(data: &[u8]) -> [u8; 16] {
 	let mut dest = [0; 16];
 	twox(&mut dest, data, 0);
@@ -65,6 +72,7 @@ pub fn twox128(data: &[u8]) -> [u8; 16] {
 	dest
 }
 
+/// Hash the data into 32-bytes with XX algorithm.
 pub fn twox256(data: &[u8]) -> [u8; 32] {
 	let mut dest = [0; 32];
 	twox(&mut dest, data, 0);
@@ -75,6 +83,7 @@ pub fn twox256(data: &[u8]) -> [u8; 32] {
 	dest
 }
 
+/// Hash the data into `twox_64(data) + data`.
 pub fn twox64_concat(data: &[u8]) -> Vec<u8> {
 	let data = data;
 	let mut v = twox64(data).to_vec();
@@ -83,10 +92,7 @@ pub fn twox64_concat(data: &[u8]) -> Vec<u8> {
 	v
 }
 
-pub fn identity(data: &[u8]) -> &[u8] {
-	data
-}
-
+/// Hash the data into 32-bytes with Keccak algorithm.
 pub fn keccak256(data: &[u8]) -> [u8; 32] {
 	let mut keccak = Keccak::v256();
 	keccak.update(data);
@@ -97,6 +103,7 @@ pub fn keccak256(data: &[u8]) -> [u8; 32] {
 	output
 }
 
+/// Hash the data into 64-bytes with Keccak algorithm.
 pub fn keccak512(data: &[u8]) -> [u8; 64] {
 	let mut keccak = Keccak::v512();
 	keccak.update(data);
@@ -107,6 +114,7 @@ pub fn keccak512(data: &[u8]) -> [u8; 64] {
 	output
 }
 
+/// Hash the data into 32-bytes with SHA2 algorithm.
 pub fn sha2_256(data: &[u8]) -> [u8; 32] {
 	let mut hasher = Sha256::new();
 	hasher.update(data);
