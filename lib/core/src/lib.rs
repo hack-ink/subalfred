d04dc0b8f4960c3187e3635f8258034f8f420b69
Xavier Lau <xavier@inv.cafe>
2022-11-13 21:19:08 +0800

    Format

--- lib/core/src/lib.rs
+++ lib/core/src/lib.rs
@@ -1,5 +1,16 @@
 //! Subalfred core libraries.
 
+pub mod prelude {
+	//! Subalfred core prelude.
+
+	pub use std::result::Result as StdResult;
+
+	pub use super::error::{self, Error};
+
+	/// Subalfred core libraries' `Result` type.
+	pub type Result<T> = StdResult<T, Error>;
+}
+
 pub mod cargo;
 pub mod check;
 pub mod error;
@@ -11,14 +22,3 @@ pub mod ss58;
 pub mod state;
 pub mod substrate_client;
 pub mod system;
-
-pub mod prelude {
-	//! Subalfred core prelude.
-
-	pub use std::result::Result as StdResult;
-
-	pub use super::error::{self, Error};
-
-	/// Subalfred core libraries' `Result` type.
-	pub type Result<T> = StdResult<T, Error>;
-}
