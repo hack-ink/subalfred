05d43df554ae9c5701fe427dfd95c5a5e6f8152b
Xavier Lau <xavier@inv.cafe>
2022-11-20 13:26:30 +0800

    Release `subspector` (#307)
    * Release `subspector`
    
    * Correct syntax

--- .maintain/release.sh
+++ .maintain/release.sh
@@ -5,16 +5,17 @@ cargo publish --locked -p subhasher
 cargo publish --locked -p submetadatan
 cargo publish --locked -p subrpcer
 cargo publish --locked -p subruntimer
+cargo publish --locked -p subspector
 # substorager depends on subhasher, sleep for 15s
-sleep 15s
+sleep 15
 cargo publish --locked -p substorager
 cargo publish --locked -p subversioner
 
 cargo publish --locked -p subalfred-util
 # subalfred-core depends on subalfred-util, sleep for 15s
-sleep 15s
+sleep 15
 cargo publish --locked -p subalfred-core
 cargo publish --locked -p cmd-impl
 # subalfred depends on cmd-impl, sleep for 15s
-sleep 15s
+sleep 15
 cargo publish --locked -p subalfred
