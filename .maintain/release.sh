78678544633c0c1ebe49159eeff1da40a344f5a2
Xavier Lau <xavier@inv.cafe>
2022-11-20 13:18:18 +0800

    Release `subspector`

--- .maintain/release.sh
+++ .maintain/release.sh
@@ -5,6 +5,7 @@ cargo publish --locked -p subhasher
 cargo publish --locked -p submetadatan
 cargo publish --locked -p subrpcer
 cargo publish --locked -p subruntimer
+cargo publish --locked -p subspector
 # substorager depends on subhasher, sleep for 15s
 sleep 15s
 cargo publish --locked -p substorager
