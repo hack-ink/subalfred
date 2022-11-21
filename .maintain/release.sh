d1cdb3592f1def6e8756669c760568e21607800a
Xavier Lau <xavier@inv.cafe>
2022-11-21 12:54:55 +0800

    Release `v0.9.0-rc19` (#314)
    * Longer sleep time
    
    * Release `v0.9.0-rc19`

--- .maintain/release.sh
+++ .maintain/release.sh
@@ -6,16 +6,16 @@ cargo publish --locked -p submetadatan
 cargo publish --locked -p subrpcer
 cargo publish --locked -p subruntimer
 cargo publish --locked -p subspector
-# substorager depends on subhasher, sleep for 15s
-sleep 15
+# substorager depends on subhasher, sleep for 30s
+sleep 30
 cargo publish --locked -p substorager
 cargo publish --locked -p subversioner
 
 cargo publish --locked -p subalfred-util
-# subalfred-core depends on subalfred-util, sleep for 15s
-sleep 15
+# subalfred-core depends on subalfred-util, sleep for 30s
+sleep 30
 cargo publish --locked -p subalfred-core
 cargo publish --locked -p cmd-impl
-# subalfred depends on cmd-impl, sleep for 15s
-sleep 15
+# subalfred depends on cmd-impl, sleep for 30s
+sleep 30
 cargo publish --locked -p subalfred
