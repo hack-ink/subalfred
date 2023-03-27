5872c29b07e455de8f8cbc834f1cf37a8e1d448d
Xavier Lau <xavier@inv.cafe>
2023-03-27 19:23:47 +0800

    Adapt vergen 8 (#441)
    * Bump vergen from 7.5.1 to 8.0.0
    
    Bumps [vergen](https://github.com/rustyhorde/vergen) from 7.5.1 to 8.0.0.
    - [Release notes](https://github.com/rustyhorde/vergen/releases)
    - [Commits](https://github.com/rustyhorde/vergen/compare/7.5.1...8.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: vergen
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Adapt vergen 8
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- bin/subalfred/build.rs
+++ bin/subalfred/build.rs
@@ -1,17 +1,19 @@
 // crates.io
-use vergen::{Config, ShaKind};
+use vergen::EmitBuilder;
 
 fn main() {
-	let mut config = Config::default();
+	let mut emitter = EmitBuilder::builder();
 
-	*config.git_mut().sha_kind_mut() = ShaKind::Short;
+	emitter.cargo_target_triple();
 
 	// Disable the git version if installed from <crates.io>.
-	if vergen::vergen(config.clone()).is_err() {
-		*config.git_mut().enabled_mut() = false;
+	if emitter.clone().git_sha(true).fail_on_error().emit().is_err() {
+		println!("cargo:rustc-env=VERGEN_GIT_SHA=crates.io");
 
-		println!("cargo:rustc-env=VERGEN_GIT_SHA_SHORT=crates.io");
-
-		vergen::vergen(config).unwrap();
+		emitter
+	} else {
+		*emitter.git_sha(true)
 	}
+	.emit()
+	.unwrap();
 }
