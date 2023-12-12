18d1bef2c6a152f60458b5673f74b8a3ed4a61d9
Xavier Lau <xavier@inv.cafe>
2023-12-13 01:07:37 +0800

    Fix tests

--- lib/core/src/jsonrpc/http/test.rs
+++ lib/core/src/jsonrpc/http/test.rs
@@ -4,7 +4,9 @@ use subrpcer::system;
 
 #[tokio::test]
 async fn send_jsonrpc_should_work() {
-	let response = send::<_, String>("https://rpc.polkadot.io", &system::chain(0)).await;
+	let response =
+		send::<_, String>("https://rpc.polkadot.io", &system::chain(0), Duration::from_secs(10))
+			.await;
 
 	assert!(response.is_ok());
 
